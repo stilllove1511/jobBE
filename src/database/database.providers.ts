15b07b8bcdb10d015631c6e1c14ddca6faae7fad
Your Name <commitment1511@outlook.com>
2023-04-08 18:22:11 +0700

    env db

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -7,11 +7,11 @@ export const databaseProviders = [
         useFactory: async () => {
             const dataSource = new DataSource({
                 type: 'mysql',
-                host: 'localhost',
-                port: 3306,
-                username: 'root',
-                password: '123456',
-                database: 'job',
+                host: process.env.DB_HOST,
+                port: parseInt(process.env.DB_PORT),
+                username: process.env.DB_USER,
+                password: process.env.DB_PASSWORD,
+                database: process.env.DB_NAME,
                 entities: [__dirname + '/../**/*.entity.{ts,js}'],
                 synchronize: true,
             })
